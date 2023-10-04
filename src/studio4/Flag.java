fe972f83e1d64e86958184981bf0fc74c6e2dd52
nick <nick@localhost>
2023-10-04 17:12:11 -0500

    Studio 4 :)

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,21 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(StdDraw.MAGENTA);
+		StdDraw.filledRectangle(0, 0, 1.0, 1.0);
+		
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledCircle(0.2, 0.2, 0.1);
+		
+		StdDraw.setPenColor(StdDraw.CYAN);
+		StdDraw.setPenRadius(0.1);
+		StdDraw.ellipse(0.65, 0.65, 0.2, 0.3);
+		StdDraw.filledRectangle(.65, 0.65, .1, 0.1);
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.rectangle(0, 0, 1, 1);
+		
+		
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,53 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double param1 = in.nextDouble();
+		double param2 = in.nextDouble();
+		double param3 = in.nextDouble();
+		double param4 = in.nextDouble();
+		
+		
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		
+		
+		if(shapeType.equals("rectangle")){
+			if(isFilled) {
+				StdDraw.filledRectangle(param1, param2, param3, param4);
+			}
+			else {
+				StdDraw.rectangle(param1, param2, param3, param4);
+			}
+		}
+		else if(shapeType.equals("ellipse")){
+			if(isFilled) {
+				StdDraw.filledEllipse(param1, param2, param3, param4);
+			}
+			else {
+				StdDraw.ellipse(param1, param2, param3, param4);
+			}
+			
+		}
+		else if(shapeType.equals("triangle")) {
+			double param5 = in.nextDouble();
+			double param6 = in.nextDouble();
+			double[] x = {param1, param3, param5};
+			double[] y = {param2, param4, param6};
+			
+			if(isFilled) {
+				StdDraw.filledPolygon(x, y);
+			}
+			else {
+				StdDraw.polygon(x, y);
+			}
+			
+		}
+		
+		
+		
 	}
 }
